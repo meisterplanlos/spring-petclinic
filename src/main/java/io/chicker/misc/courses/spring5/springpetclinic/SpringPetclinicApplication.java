4e78db2a2331d01b303f849826e16656036e2400
Holger Becker <holger.becker@telespazio-vega.de>
2018-08-31 21:32:30 +0200

    Initial commit

---
+++ src/main/java/io/chicker/misc/courses/spring5/springpetclinic/SpringPetclinicApplication.java
@@ -0,0 +1,20 @@
+package io.chicker.misc.courses.spring5.springpetclinic;
+
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+
+@SpringBootApplication
+public class SpringPetclinicApplication
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(SpringPetclinicApplication.class, args);
+  }
+}
